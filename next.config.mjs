144ea65afa47ffa82de52bedd2f80216b8eb7248
Sachintha <sudesh@orelit.com>
2025-09-20 17:17:01 +0530

    fix: update basePath configuration and ensure images are unoptimized in production

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
     unoptimized: true,
   },
   output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
